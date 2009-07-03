3c0a29b0a1eb789ab74bc65a53e5d0d7954f567c
Andrew Ng <ayn@andrewng.com>
2009-07-02 18:46:12 -0700

    reversing the async logic

--- lib/facebooker_session_patch.rb
+++ lib/facebooker_session_patch.rb
@@ -4,8 +4,8 @@ module Facebooker
   
   class Session
     def post_with_async(method, params = {}, use_session = true, &proc)
-      never_queue = %w(facebook.auth.getSession facebook.auth.createToken facebook.application.getPublicInfo)
-      if never_queue.include?(method) || batch_request? || !queue? || !(qsa = self.queue_service_adapter)
+      async_methods = %w(facebook.feed.publishUserAction facebook.profile.setFBML facebook.notifications.sendEmail facebook.notifications.send)
+      if !async_methods.include?(method) || batch_request? || !queue? || !(qsa = self.queue_service_adapter)
         self.post_without_async(method, params, use_session, &proc)
       else
         qsa.put(
